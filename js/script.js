875147b3e0c0866f316ed40367f91bcd8ced57c6
Tareq <tareqitos@gmail.com>
2024-02-03 19:00:54 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,78 +1,129 @@
 window.onscroll = function () {
-    ShowNavButtons();
+  ShowNavButtons()
 }
 
-function ShowNavButtons() {
-    const nav = document.querySelector(".project-nav");
-    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    if (nav == null) {
-        return
+document.addEventListener('DOMContentLoaded', function () {
+    const zooming = new Zooming()
+    zooming.listen('img')
+    
+    zooming.config({
+        scaleBase: .5
+      })
+    zooming.config({
+        bgOpacity: .8
+      })
+  })
+
+
+function ShowNavButtons () {
+  const nav = document.querySelector('.project-nav')
+  const width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth
+
+  if (nav == null) {
+    return
+  }
+
+  if (document.documentElement.scrollTop > 300) {
+    if (width > 1024) {
+      setTimeout(() => {
+        nav.style.opacity = 1
+      }, 10)
+      nav.style.display = 'inherit'
     }
+  }
 
-    if (document.documentElement.scrollTop > 300) {
-        if (width > 1024) {
-            setTimeout(() => {
-                nav.style.opacity = 1
-            }, 10)
-            nav.style.display = "inherit"
-        } 
-    } 
-
-    if (document.documentElement.scrollTop < 150) {
-        setTimeout(() => {
-            nav.style.display = "none"
-        }, 200)
-        nav.style.opacity = 0
-    }
-
+  if (document.documentElement.scrollTop < 150) {
+    setTimeout(() => {
+      nav.style.display = 'none'
+    }, 200)
+    nav.style.opacity = 0
+  }
 }
 
-$(document).ready(function() {
-    initPage();
+function playPauseVideo () {
+  let videos = document.querySelectorAll('video')
+  videos.forEach(video => {
+    video.muted = true
 
-    // Gestionnaire d'événements pour détecter la réaffichage de la page depuis le cache du navigateur
-    $(window).on("pageshow", function(event) {
-        // Vérifie si l'événement est causé par le bouton précédent
-        if (event.originalEvent.persisted) {
-            initPage();
-        }
-    });
+    let observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > 0.2) {
+            // Video is at least 20% visible
+            if (video.paused) {
+              video.play()
+            }
+          } else {
+            // Video is less than 20% visible
+            if (!video.paused) {
+              video.pause()
+            }
+          }
+        })
+      },
+      { threshold: [0, 0.2] } // Added 0 threshold to handle edge cases
+    )
 
-    $("a.transition").click(function(event) {
-        event.preventDefault();
-        linkLocation = this.href;
+    observer.observe(video)
 
-        $("main").fadeOut(250, function() {
-            $("main").css("display", "none");
-            $("footer").css("display", "none");
-            redirectPage();
-        });
+    // Pause the video when it's not in the viewport
+    video.addEventListener('pause', () => {
+      video.currentTime = 0 // Rewind the video to the beginning when paused
+    })
+  })
+}
 
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  playPauseVideo()
+})
 
-    $("a.transition-project").click(function(event) {
-        event.preventDefault();
-        linkLocation = this.href;
+$(document).ready(function () {
+  initPage()
 
-        $("main").fadeOut(250, function() {
-            $("main").css("display", "none");
-            $("footer").css("display", "none");
-            redirectPage();
-        });
-
-        $(".portfolio-items").css("transform", "scale(.7)");
-    });
-
-
-    function redirectPage() {
-        window.location = linkLocation;
+  // Gestionnaire d'événements pour détecter la réaffichage de la page depuis le cache du navigateur
+  $(window).on('pageshow', function (event) {
+    // Vérifie si l'événement est causé par le bouton précédent
+    if (event.originalEvent.persisted) {
+      initPage()
     }
+  })
 
-    function initPage() {
-        $("main").css("display", "none");
-        $("footer").css("display", "block");
-        $(".portfolio-items").css("transform", "scale(1)");
-        $("main").fadeIn(600);
-    }
-});
+  $('a.transition').click(function (event) {
+    event.preventDefault()
+    linkLocation = this.href
+
+    $('main').fadeOut(250, function () {
+      $('main').css('display', 'none')
+      $('footer').css('display', 'none')
+      redirectPage()
+    })
+  })
+
+  $('a.transition-project').click(function (event) {
+    event.preventDefault()
+    linkLocation = this.href
+
+    $('main').fadeOut(250, function () {
+      $('main').css('display', 'none')
+      $('footer').css('display', 'none')
+      redirectPage()
+    })
+
+    $('.portfolio-items').css('transform', 'scale(.7)')
+  })
+
+  function redirectPage () {
+    window.location = linkLocation
+  }
+
+  function initPage () {
+    $('main').css('display', 'none')
+    $('footer').css('display', 'block')
+    $('.portfolio-items').css('transform', 'scale(1)')
+    $('main').fadeIn(600)
+  }
+})
